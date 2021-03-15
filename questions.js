30e81947a281cf7ba0ee6f5c357929eb911bf586
Monika2006 <62497774+Monika2006@users.noreply.github.com>
2021-03-15 23:25:01 +0530

    Quiz game

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is father of C Langauge?",
+    answer: "Dennis Ritchie",
+    options: [
+      "Bjarne Stroustrup",
+      "James A.Gosling",
+      "Dennis Ritchie",
+      "Dr.E.F.Codd"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Why is a macro used in place of a function?",
+    answer: "It increases code size",
+    options: [
+      "It reduces execution time",
+      "It reduces code size",
+      "It increases execution time.",
+      "It increases code size"
+    ]
+  },
+    {
+    numb: 3,
+    question: " In the C language, the constant is defined ____?",
+    answer: "Anywhere, but starting on a new line",
+    options: [
+      "Before main",
+      "After main",
+      "Anywhere, but starting on a new line",
+      "None of the these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What feature makes C++ so powerful?",
+    answer: "All of the above",
+    options: [
+      "Easy implementation",
+      "Reusing the old code",
+      "Easy memory management",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following SLT template class is a container adaptor class?",
+    answer: "Stack",
+    options: [
+      "Stack",
+      "List",
+      "Deque",
+      "Vector"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
